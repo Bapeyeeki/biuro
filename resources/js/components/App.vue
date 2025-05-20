6fa75d1f92f337585ac422a8bff0f769009b837e
DawidLD <dawidek.l@interia.pl>
2025-05-20 11:42:47 +0200

    dane

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -298,24 +298,6 @@
               </div>
             </div>
             <div>
-              <h3 class="text-lg font-bold mb-4 text-white">Biura</h3>
-              <ul class="space-y-2 text-gray-400">
-                <li><a href="#" class="hover:text-blue-400 transition">Dostępne biura</a></li>
-                <li><a href="#" class="hover:text-blue-400 transition">Wirtualne biura</a></li>
-                <li><a href="#" class="hover:text-blue-400 transition">Przestrzenie coworkingowe</a></li>
-                <li><a href="#" class="hover:text-blue-400 transition">Sale konferencyjne</a></li>
-              </ul>
-            </div>
-            <div>
-              <h3 class="text-lg font-bold mb-4 text-white">O nas</h3>
-              <ul class="space-y-2 text-gray-400">
-                <li><a href="#" class="hover:text-blue-400 transition">Nasza firma</a></li>
-                <li><a href="#" class="hover:text-blue-400 transition">Lokalizacje</a></li>
-                <li><a href="#" class="hover:text-blue-400 transition">Kariera</a></li>
-                <li><a href="#" class="hover:text-blue-400 transition">Blog</a></li>
-              </ul>
-            </div>
-            <div>
               <h3 class="text-lg font-bold mb-4 text-white">Kontakt</h3>
               <ul class="space-y-2 text-gray-400">
                 <li class="flex items-start">
@@ -323,19 +305,19 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                   </svg>
-                  <span>ul. Warszawska 142, 02-318 Warszawa</span>
+                  <span>ul. Armii Krajowej 9A, 41-506 Chorzów</span>
                 </li>
                 <li class="flex items-start">
                   <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2 text-blue-400 mt-0.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                   </svg>
-                  <span>kontakt@arrisagency.pl</span>
+                  <span>kontakt@arris.pl</span>
                 </li>
                 <li class="flex items-start">
                   <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2 text-blue-400 mt-0.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                   </svg>
-                  <span>+48 22 123 45 67</span>
+                  <span>+48 691 661 859</span>
                 </li>
               </ul>
             </div>
